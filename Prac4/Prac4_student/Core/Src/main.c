f88c9bc1eca37474e1e469fad86ff8ab6977ec91
Simbarashe Mawere <20simbarashem@gmail.com>
2023-10-18 19:31:17 +0200

    done with the code. Now just the bread board left

--- Prac4/Prac4_student/Core/Src/main.c
+++ Prac4/Prac4_student/Core/Src/main.c
@@ -34,9 +34,10 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 // TODO: Add values for below variables
-#define NS        // Number of samples in LUT
-#define TIM2CLK   // STM Clock frequency
-#define F_SIGNAL  // Frequency of output analog signal
+// TASK 2: Define values for NS, TIM2CLK, and F_SIGNAL
+#define NS        128// Number of samples in LUT
+#define TIM2CLK   8000000// STM Clock frequency
+#define F_SIGNAL  1000// Frequency of output analog signal
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -51,16 +52,78 @@ DMA_HandleTypeDef hdma_tim2_ch1;
 
 /* USER CODE BEGIN PV */
 // TODO: Add code for global variables, including LUTs
+// TASK 1: Create LUTs for Sine, Sawtooth, and Triangle waves
+uint32_t Sin_LUT[NS] = {
+  512,537,562,587,611,636,660,684,
+  707,730,753,774,796,816,836,855,
+  873,890,907,922,937,950,963,974,
+  984,993,1001,1008,1013,1017,1021,1022,
+  1023,1022,1021,1017,1013,1008,1001,993,
+  984,974,963,950,937,922,907,890,
+  873,855,836,816,796,774,753,730,
+  707,684,660,636,611,587,562,537,
+  512,486,461,436,412,387,363,339,
+  316,293,270,249,227,207,187,168,
+  150,133,116,101,86,73,60,49,
+  39,30,22,15,10,6,2,1,
+  0,1,2,6,10,15,22,30,
+  39,49,60,73,86,101,116,133,
+  150,168,187,207,227,249,270,293,
+  316,339,363,387,412,436,461,486
+};
 
-uint32_t Sin_LUT[NS] = {};
+uint32_t saw_LUT[NS] = {
+  8,16,24,32,40,48,56,64,
+  72,80,88,96,104,112,120,128,
+  136,144,152,160,168,176,184,192,
+  200,208,216,224,232,240,248,256,
+  264,272,280,288,296,304,312,320,
+  328,336,344,352,360,368,376,384,
+  392,400,408,416,424,432,440,448,
+  456,464,472,480,488,496,504,512,
+  519,527,535,543,551,559,567,575,
+  583,591,599,607,615,623,631,639,
+  647,655,663,671,679,687,695,703,
+  711,719,727,735,743,751,759,767,
+  775,783,791,799,807,815,823,831,
+  839,847,855,863,871,879,887,895,
+  903,911,919,927,935,943,951,959,
+  967,975,983,991,999,1007,1015,1023,
+};
 
-uint32_t saw_LUT[NS] = {};
+uint32_t triangle_LUT[NS] = {
+  16,32,48,64,80,96,112,128,
+  144,160,176,192,208,224,240,256,
+  272,288,304,320,336,352,368,384,
+  400,416,432,448,464,480,496,512,
+  527,543,559,575,591,607,623,639,
+  655,671,687,703,719,735,751,767,
+  783,799,815,831,847,863,879,895,
+  911,927,943,959,975,991,1007,1023,
+  1007,991,975,959,943,927,911,895,
+  879,863,847,831,815,799,783,767,
+  751,735,719,703,687,671,655,639,
+  623,607,591,575,559,543,527,512,
+  496,480,464,448,432,416,400,384,
+  368,352,336,320,304,288,272,256,
+  240,224,208,192,176,160,144,128,
+  112,96,80,64,48,32,16,0,
+};
 
-uint32_t triangle_LUT[NS] = {};
+// setting up indexing for the types of the waveforms
+char* waveforms[3] = {"Sine", "Saw", "Triangle"};
+uint32_t *LUT[3] = {Sin_LUT, saw_LUT, triangle_LUT};
+uint32_t LUT_index = 0;
 
 // TODO: Equation to calculate TIM2_Ticks
-uint32_t TIM2_Ticks = 0; // How often to write new LUT value
+// TASK 3: Calculate TIM2_Ticks to generate F_SIGNAL
+uint32_t TIM2_Ticks = TIM2CLK/(F_SIGNAL*NS); // How often to write new LUT value
 uint32_t DestAddress = (uint32_t) &(TIM3->CCR3); // Write LUT TO TIM3->CCR3 to modify PWM duty cycle
+
+
+// setting up variables for debouncing
+uint32_t last_debounce_time = 0;
+uint32_t debounce_delay = 500; // in ms
 
 /* USER CODE END PV */
 
@@ -111,20 +174,23 @@ int main(void)
   MX_TIM3_Init();
 
   /* USER CODE BEGIN 2 */
+  // TASK 4: setting up the PWM and OC for TIM3 and TIM2
   // TODO: Start TIM3 in PWM mode on channel 3
-
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
 
   // TODO: Start TIM2 in Output Compare (OC) mode on channel 1.
-
+  HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_1);
 
   // TODO: Start DMA in IT mode on TIM2->CH1; Source is LUT and Dest is TIM3->CCR3; start with Sine LUT
-
+  HAL_DMA_Start_IT(&hdma_tim2_ch1, LUT[LUT_index], DestAddress, NS);
 
   // TODO: Write current waveform to LCD ("Sine")
   delay(3000);
+  lcd_command(CLEAR);
+  lcd_putstring(waveforms[LUT_index]);
 
   // TODO: Enable DMA (start transfer from LUT to CCR)
-
+  __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
 
   /* USER CODE END 2 */
 
@@ -350,12 +416,28 @@ static void MX_GPIO_Init(void)
 void EXTI0_1_IRQHandler(void)
 {
 	// TODO: Debounce using HAL_GetTick()
-
+  // TASK 5: Debounce button press and change waveform
+  if (HAL_GetTick() - last_debounce_time > debounce_delay) {
+    last_debounce_time = HAL_GetTick();
+    LUT_index = (LUT_index + 1) % 3; // increment LUT_index and wraps around
+  }
+  else {
+    // just debouncing
+    HAL_GPIO_EXTI_IRQHandler(Button0_Pin); // Clear interrupt flags
+    return;
+  }
 
 	// TODO: Disable DMA transfer and abort IT, then start DMA in IT mode with new LUT and re-enable transfer
 	// HINT: Consider using C's "switch" function to handle LUT changes
+  __HAL_TIM_DISABLE_DMA(&htim2, TIM_DMA_CC1);
+  HAL_DMA_Abort_IT(&hdma_tim2_ch1);
 
+  HAL_DMA_Start_IT(&hdma_tim2_ch1, LUT[LUT_index], DestAddress, NS);
+  __HAL_TIM_ENABLE_DMA(&htim2, TIM_DMA_CC1);
 
+  // TASK 6: Rewrite LCD with new waveform
+  lcd_command(CLEAR);
+  lcd_putstring(waveforms[LUT_index]);
 
 	HAL_GPIO_EXTI_IRQHandler(Button0_Pin); // Clear interrupt flags
 }
